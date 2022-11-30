f686f744b5c641ea79495847eaf0eb985163cd48
BangaruNaiduEarle <115337924+BangaruNaiduEarle@users.noreply.github.com>
2022-11-30 14:31:24 +0530

    Update index.js

--- src/components/ThumbnailItem/index.js
+++ src/components/ThumbnailItem/index.js
@@ -3,19 +3,22 @@
 import './index.css'
 
 const ThumbnailItem = props => {
-  const {eachImgEle, onClickThumbnailImgEle} = props
+  const {eachImgEle, onClickThumbnailImgEle, isActive} = props
 
   const {id, thumbnailUrl, thumbnailAltText} = eachImgEle
 
   const onclickThumbnail = () => {
     onClickThumbnailImgEle(id)
   }
-
+  
+  const ImageStyle = isActive ? 'thumbnail-img' : ''
+  
   return (
-    <li className="list-items">
-      <button type="button" className="button" onClick={onclickThumbnail}>
-        <img src={thumbnailUrl} alt={thumbnailAltText} className="list-imgs" />
-      </button>
+       <li className={`list-items ${ImageStyle}`}>
+
+        <button type="button" className="button" onClick={onclickThumbnail}>
+          <img src={thumbnailUrl} alt={thumbnailAltText} className="list-imgs" />
+        </button>
     </li>
   )
 }
